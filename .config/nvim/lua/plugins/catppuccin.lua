76f7b4617fce0856376a1a19947d8d12d8773e62
Angelo Mendes <angelo030825@gmail.com>
2026-04-13 12:58:30 -0300

    feat: add automatic switch dark and light theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,3 +1,27 @@
+local function is_dark_mode()
+  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+  if not handle then
+    return true
+  end
+
+  local result = handle:read("*a")
+  handle:close()
+
+  return result:match("Dark") ~= nil
+end
+
+local function apply_theme()
+  local dark = is_dark_mode()
+
+  if dark then
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  else
+    vim.o.background = "light"
+    vim.cmd.coloscheme("catppuccin-latte")
+  end
+end
+
 return {
   {
     "catppuccin/nvim",
@@ -6,5 +30,14 @@ return {
     opts = {
       flavour = "macchiato",
     },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+
+      apply_theme()
+
+      vim.api.nvim_create_autocmd({ "VimEnter", "FocusGained" }, {
+        callback = apply_theme,
+      })
+    end,
   },
 }
