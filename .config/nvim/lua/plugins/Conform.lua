68dc302ed260c6e421b1201c803b3d74b50de443
Angelo Mendes <angelo030825@gmail.com>
2026-07-16 03:49:10 -0300

    refactor: delete `none-ls` and merge config into `Conform.lua`

--- .config/nvim/lua/plugins/Conform.lua
+++ .config/nvim/lua/plugins/Conform.lua
@@ -2,14 +2,30 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("conform").setup({
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_format" },
+				go = { "goimports", "gofmt" },
 				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
 				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				scss = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				svelte = { "prettier" },
 			},
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
 		})
+
+		vim.keymap.set("n", "<leader>gf", function()
+			conform.format({ async = true, lsp_fallback = true })
+		end, { desc = "Format current buffer" })
 	end,
 }
